c69590f72f42b8473187c1e5988dbe0b59d3542f
Nathan Dench <ndenc2@gmail.com>
2015-06-19 19:52:55 +1000

    Finish week2 ex2

--- src/week2/HW02.hs
+++ src/week2/HW02.hs
@@ -12,8 +12,10 @@ type Code = [Peg]
 
 -- A move is constructed using a Code and two integers; the number of
 -- exact matches and the number of regular matches
-data Move = Move Code Int Int
-          deriving (Show, Eq)
+data Move = Move  { code :: Code
+                  , exact :: Int
+                  , regular :: Int
+                  } deriving (Show, Eq)
 
 -- List containing all of the different Pegs
 colors :: [Peg]
@@ -33,29 +35,57 @@ exactMatches (x:xs) (y:ys)
 
 -- For each peg in xs, count how many times is occurs in ys
 frequency :: Eq a => a -> [a] -> Int
-frequency x [] = 0
+frequency _ [] = 0
 frequency x (y:ys)
     | x == y    = 1 + frequency x ys
     | otherwise = frequency x ys
 
+-- Count how many times each color appears in the code
 countColors :: Code -> [Int]
---countColors c = map (frequency c) colors
 countColors x = [frequency c x | c <- colors]
+
+-- Zip up the color count of two codes
+zipCountCodes :: Code -> Code -> [(Int, Int)]
+zipCountCodes c1 c2 =
+    let
+        c1Count = countColors c1
+        c2Count = countColors c2
+    in
+        zip c1Count c2Count
+
+-- Find the minimum of each tuple in a list of touples
+mapTupleMin :: [(Int, Int)] -> [Int]
+mapTupleMin [] = []
+mapTupleMin ((x,y):zs) = min x y : mapTupleMin zs
 
 -- Count number of matches between the actual code and the guess
 matches :: Code -> Code -> Int
-matches = undefined
+matches c1 c2 =
+    let
+        zippedCounts = zipCountCodes c1 c2
+        mins         = mapTupleMin zippedCounts
+    in
+        sum mins
 
 -- Exercise 3 -----------------------------------------
 
 -- Construct a Move from a guess given the actual code
 getMove :: Code -> Code -> Move
-getMove = undefined
+getMove secret guess =
+    let
+        exact   = exactMatches secret guess
+        regular = (matches secret guess) - exact
+    in
+        Move guess exact regular
 
 -- Exercise 4 -----------------------------------------
 
 isConsistent :: Move -> Code -> Bool
-isConsistent = undefined
+isConsistent move code =
+    let
+        guess = code move
+        guessExact = exact move
+        guessRegular = regular move
 
 -- Exercise 5 -----------------------------------------
 
